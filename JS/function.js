8627ef0ce01a7b484ad5310ff21e8ba4b9e3f67f
yuukihana <avoynova00@gmail.com>
2024-06-29 17:04:11 +0200

    Created grandma modal Pop-up

---
+++ JS/function.js
@@ -0,0 +1,18 @@
+const modal = document.querySelector(".my-modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open-modal");
+const closeModalBtn = document.querySelector(".btn-close-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+closeModalBtn.addEventListener("click", closeModal);
