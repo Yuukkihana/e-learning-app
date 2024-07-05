4702a641e38713cfd675f786c4dba7c083e0400b
yuukihana <avoynova00@gmail.com>
2024-07-05 17:13:08 +0200

    added quiz to magen/darm module

--- JS/quizz-magen.js
+++ JS/quizz-magen.js
@@ -1,6 +1,7 @@
 //Quizz
 const questionElement = document.getElementById("question");
-const answerButtonsElement = document.getElementById("answer-buttons")
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next");
 
 const questions = [{
     question: "Warum sind Kamille und Kümmel gut bei Bauchschmerzen?",
@@ -44,6 +45,80 @@ const questions = [{
           ]}
   ];
 
+  let currentQuestionIndex = 0;
+  let score = 0;
+
   function showQuestion(question){
-    questionElement.innerText = question.question;
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNumber = currentQuestionIndex + 1;
+    questionElement.innerHTML = currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer =>  {
+      const button = document.createElement("button");
+      button.innerHTML = answer.text;
+      button.classList.add("btns");
+      answerButton.appendChild(button);
+      if(answer.correct){
+        button.dataset.correct = answer.correct; //addiert "true" oder "false"
+      }
+      button.addEventListener("click", selectAnswer);
+    });
   }
+
+  function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Nächste Frage";
+    showQuestion();
+  }
+
+function resetState(){
+  nextButton.style.display = "none"
+  while(answerButton.firstChild){
+    answerButton.removeChild(answerButton.firstChild);
+  }
+}
+
+function selectAnswer(e){
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true"; //kontrolliert ob dataset ist true
+  if(isCorrect){
+    selectedBtn.classList.add("correct"); //addiert class basiert auf dataset true or false
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButton.children).forEach(button => {
+    if(button.dataset.correct === "true"){
+      button.classList.add("correct");
+    }
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore(){
+  resetState();
+  questionElement.innerHTML = `Du hast ${score} von ${questions.length} Fragen richtig beantwortet!`
+  nextButton.innerHTML = "Nochmal";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+  currentQuestionIndex++;
+  if(currentQuestionIndex < questions.length){
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", ()=>{
+  if(currentQuestionIndex < questions.length){
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+
+startQuiz();
